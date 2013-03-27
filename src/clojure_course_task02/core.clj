6a553a157b6dcb74e88c0173d391f46b8943aee8
EvgenSk <evgensk@gmail.com>
2013-03-27 10:29:58 +0700

    just a billet

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,6 +1,17 @@
 (ns clojure-course-task02.core
   (:gen-class))
 
+(defn raw-dirs-walker [func root]
+  (do
+    (func root)
+    (if (.isDirectory root)
+      (->> (file-seq root)
+           (rest)
+           (map #(raw-dirs-walker func %))))))
+
+(defn dirs-walker [func root]
+  (raw-dirs-walker #(func (.getName %)) 
+                   (clojure.java.io/file root)))
 
 (defn find-files [file-name path]
   "TODO: Implement searching for a file using his name as a regexp."
